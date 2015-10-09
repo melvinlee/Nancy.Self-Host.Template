79bd9fa2d767c2bccc35793e6c67078300b52faa
Melvin Lee <melvinlee@hotmail.com>
2015-10-09 12:47:20 +0800

    Add Mspec test.

---
+++ src/Nancy.Self-Host.Template.Specs/When_get_on_home_module.cs
@@ -0,0 +1,27 @@
+﻿using Machine.Specifications;
+using Nancy.SelfHost.Template;
+using Nancy.Testing;
+
+namespace Nancy.Self_Host.Template.Specs
+{
+    public class When_get_on_home_module
+    {
+        private static Browser _browser;
+        private static BrowserResponse _browserResponse;
+
+        Establish context = () =>
+        {
+            var bootstrapper = new ConfigurableBootstrapper(with => with.Module<HomeModule>());
+            _browser = new Browser(bootstrapper);
+        };
+
+        Because of = () =>
+        {
+            _browserResponse = _browser.Get("/", with => with.HttpRequest());
+        };
+
+        It should_return_http_ok = () => _browserResponse.StatusCode.ShouldEqual(HttpStatusCode.OK);
+
+        It should_return_hello_world = () => _browserResponse.Body.AsString().ShouldEqual("Hello world!");
+    }
+}
---
+++ src/Nancy.Self-Host.Template.Specs/When_say_hello_on_home_module.cs
@@ -0,0 +1,27 @@
+﻿using Machine.Specifications;
+using Nancy.SelfHost.Template;
+using Nancy.Testing;
+
+namespace Nancy.Self_Host.Template.Specs
+{
+    public class When_say_hello_on_home_module
+    {
+        private static Browser _browser;
+        private static BrowserResponse _browserResponse;
+
+        Establish context = () =>
+        {
+            var bootstrapper = new ConfigurableBootstrapper(with => with.Module<HomeModule>());
+            _browser = new Browser(bootstrapper);
+        };
+
+        Because of = () =>
+        {
+            _browserResponse = _browser.Get("/sayhello", with => with.HttpRequest());
+        };
+
+        It should_return_http_ok = () => _browserResponse.StatusCode.ShouldEqual(HttpStatusCode.OK);            
+        
+        It should_return_hello_from_owin = () => _browserResponse.Body.AsString().ShouldEqual("Hello from Owin!");
+    }
+}
